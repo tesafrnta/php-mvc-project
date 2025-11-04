0b144c0444620ec97802f960606868ddf2910483
Tesa Firna Ananta <h1101241057@student.untan.ac.id>
2025-11-04 18:29:24 +0700

    new

--- public/index.php
+++ public/index.php
@@ -1,10 +1,21 @@
 <?php
-// Autoloader sederhana
+// Fallback untuk file statis
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$file = __DIR__ . $path;
+if (file_exists($file) && is_file($file)) {
+    return false;
+}
+
+// Tampilkan error saat debug
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Autoloader
 spl_autoload_register(function($class) {
     $classPath = str_replace(['App\\', '\\'], ['', '/'], $class);
-    $baseDir = dirname(__DIR__) . '/App/'; // tetap ke folder app dari public/
+    $baseDir = dirname(__DIR__) . '/App/';
     $file = $baseDir . $classPath . '.php';
-    
     if (file_exists($file)) {
         require_once $file;
     } else {
@@ -12,18 +23,14 @@ spl_autoload_register(function($class) {
     }
 });
 
-// Load core files
+// Load core
 require_once __DIR__ . '/../App/Core/Database.php';
 require_once __DIR__ . '/../App/Core/Router.php';
 
-// Inisialisasi Router
+// Routing
 $router = new Router();
-
-// Define Routes
 $router->get('/', 'HomeController@index');
 $router->get('/users', 'UserController@index');
 $router->get('/users/{id}', 'UserController@show');
 $router->post('/users', 'UserController@store');
-
-// Resolve request
 $router->resolve();
